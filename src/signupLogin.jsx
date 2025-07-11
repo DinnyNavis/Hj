16a0cd194ff2699b3a1810708e101d454ce947cc
Dinny Paul Navis <165354946+DinnyNavis@users.noreply.github.com>
2025-07-11 12:15:03 +0530

    Update signupLogin.jsx

--- src/signupLogin.jsx
+++ src/signupLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://growlify-baclend.onrender.com';
 
 function SignupLogin({ onNavigate }) {
     const [isLoginView, setIsLoginView] = useState(false);
