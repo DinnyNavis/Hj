711813998e182f9f13a5b001c0b2c3a54aa5d118
Dinny Paul Navis <165354946+DinnyNavis@users.noreply.github.com>
2025-07-11 13:06:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/growlify');
+        await mongoose.connect('mongodb+srv://dinnynavis:34pw2mjFslkKogSc@growlify.zhwqhgm.mongodb.net/');
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
